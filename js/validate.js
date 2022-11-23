3ec771e0d7d463c322eef391741d06092076a29d
Xurxo González <xgonten.innobosque.gmail.com>
2022-11-23 13:23:35 +0100

    Imagen de error puesto como backgroun-image en los controles de formulario

--- js/validate.js
+++ js/validate.js
@@ -1,13 +1,14 @@
 "use strict";
 
 const myForm = document.querySelector("#my-form");
+const btn = document.querySelector(".btn");
 
 // myForm.onsubmit = e => {
 //     e.preventDefault();
 
 // }
 
-myForm.onclick = () => {
+btn.onclick = () => {
     document.querySelectorAll("[required]").forEach(element=>{
         element.classList.add('required');
     })
